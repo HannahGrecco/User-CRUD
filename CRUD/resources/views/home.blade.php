9f47675d19cc9569be9b9f9e3af73ce9af714879
Hannah <grecco327@gmail.com>
2025-10-21 09:13:12 -0300

    adding responsive layout to the initial page

--- CRUD/resources/views/home.blade.php
+++ CRUD/resources/views/home.blade.php
@@ -6,13 +6,14 @@
 
     @auth
     <p>Congrats, youre login</p>
+    
     <form action="/logout" method="POST">
             @csrf
             <button>Log out</button>
     </form>
 
     <div>
-        <h2>Create a new post</h2>
+        <h2 class="bg-amber-400">Create a new post</h2>
         <form action="/create-post" method="POST">
             @csrf
             <input type="text" name='title' placeholder='Post title'>
@@ -34,25 +35,42 @@
     </div>
 
     @else
-     <div>
+    <div class="min-h-screen flex flex-col md:flex-row items-center justify-center space-y-8 md:space-y-0 md:space-x-8 lg:space-x-24 bg-gray-300">
+        <div class="border-2 bg-gray-100 p-24 rounded-lg shadow-lg">
         <h1>Register</h1>
-        <form action="/register" method="POST">
-            @csrf
-            <input name='name' type="text" placeholder="Name">
-            <input name='email' type="email" placeholder="Email">
-            <input name='password' type="password" placeholder="Password">
-            <button type="submit">Submit</button>
+            <form action="/register" method="POST">
+            <fieldset class="fieldset">
+                 @csrf
+            <input class="input mb-4" name='name' type="text" placeholder="Name">
+            <input class="input mb-4" name='email' type="email" placeholder="Email">
+            <input class="input mb-4" name='password' type="password" placeholder="Password">
+            <button class="btn btn-primary" type="submit">Submit</button>
+            </fieldset>
+           
         </form>
+        
     </div>
-    <div>
+    <div class="border-2 bg-gray-100 p-31 rounded-lg shadow-lg">
         <h1>Log-in</h1>
+        <!--se dados estiverem incorretos -> mostra mensagem de dados incorretos para o usuário-->
+        @if($errors->any())
+            <div style="color: red;">
+            {{ $errors->first('loginname') }}
+            </div>
+        @endif
+        
         <form action="/login" method="POST">
-            @csrf
-            <input name='loginname' type="text" placeholder="Name">
-            <input name='loginpassword' type="password" placeholder="Password">
-            <button type="submit">Login</button>
+            <fieldset class="fieldset  ">
+                 @csrf
+            <input class="input mb-4" name='loginname' type="text" placeholder="Name">
+            <input class="input mb-4" name='loginpassword' type="password" placeholder="Password">
+            <button class="btn btn-primary" type="submit">Login</button>
+            </fieldset>
+           
         </form>
     </div>
+    </div>
+     
     @endauth
 
    
