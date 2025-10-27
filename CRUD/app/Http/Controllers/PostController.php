00bb2776321a49757fe6ec1628360ae90cdabc94
Hannah <grecco327@gmail.com>
2025-10-27 18:15:33 -0300

    feature deletePost

--- CRUD/app/Http/Controllers/PostController.php
+++ CRUD/app/Http/Controllers/PostController.php
@@ -6,7 +6,14 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
-{
+{   
+    public function deletePost (Post $post){
+        if (auth()->user()-> id === $post->user_id) {
+            $post->delete();
+        }
+        return redirect('/');
+    }
+
     public function updatePost(Post $post, Request $request){
          if (auth()->user()-> id !== $post->user_id) {
             return redirect('/');
